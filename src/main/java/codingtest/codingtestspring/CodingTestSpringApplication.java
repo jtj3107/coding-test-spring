076e70cba83ce3204acd60d35c687a51374595d6
조태중 <jtj3926@gmail.com>
2023-12-09 16:39:02 +0900

    spring 설정

---
+++ src/main/java/codingtest/codingtestspring/CodingTestSpringApplication.java
@@ -0,0 +1,13 @@
+package codingtest.codingtestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codingtest/codingtestspring/CodingTestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package codingtest.codingtestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
