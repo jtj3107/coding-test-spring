931bc2bf211e5f02a1954b2e8a421996efae3ae1
조태중 <jtj3926@gmail.com>
2023-12-11 16:38:39 +0900

    코드 작성6

--- src/main/java/codingtest/codingtestspring/CodingTestSpringApplication.java
+++ src/main/java/codingtest/codingtestspring/CodingTestSpringApplication.java
@@ -2,8 +2,9 @@ package codingtest.codingtestspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CodingTestSpringApplication {
 
 	public static void main(String[] args) {
