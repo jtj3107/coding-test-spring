010c090e52875c4475c663432ec57858007ad7b5
조태중 <jtj3926@gmail.com>
2023-12-11 18:20:32 +0900

    코드 작성15

--- src/main/java/codingtest/codingtestspring/controller/LoginController.java
+++ src/main/java/codingtest/codingtestspring/controller/LoginController.java
@@ -1,4 +1,5 @@
 package codingtest.codingtestspring.controller;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +8,15 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "login"; // login.html 템플릿을 반환
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        // 세션 무효화
+        session.invalidate();
+
+        // 로그아웃 후 리디렉션할 페이지 (예: 로그인 페이지)
+        return "redirect:/Home";
     }
 
     @GetMapping("/signup")
