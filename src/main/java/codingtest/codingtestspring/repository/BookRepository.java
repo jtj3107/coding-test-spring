dd66ffb4359ae61d57fa2f967f336f2fca43077e
조태중 <jtj3926@gmail.com>
2023-12-11 18:07:43 +0900

    코드 작성14

--- src/main/java/codingtest/codingtestspring/repository/BookRepository.java
+++ src/main/java/codingtest/codingtestspring/repository/BookRepository.java
@@ -3,6 +3,18 @@ package codingtest.codingtestspring.repository;
 import codingtest.codingtestspring.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    // 필요한 쿼리 메소드
+    // 특정 상태의 도서 조회
+    List<Book> findByStatus(String status);
+
+    // 제목으로 도서 검색
+    List<Book> findByTitleContaining(String title);
+
+    // 저자 이름으로 도서 검색
+    List<Book> findByAuthorContaining(String author);
+
+    // ISBN으로 도서 검색
+    Book findByIsbn(String isbn);
 }
