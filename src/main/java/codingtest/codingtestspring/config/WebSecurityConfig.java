72fdef2e022068142e613ce8f64aa1fe4ccb7f1e
조태중 <jtj3926@gmail.com>
2023-12-11 17:15:02 +0900

    코드 작성9

--- src/main/java/codingtest/codingtestspring/config/WebSecurityConfig.java
+++ src/main/java/codingtest/codingtestspring/config/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package codingtest.codingtestspring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,6 +15,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     @Bean
     public SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
         http
@@ -33,7 +39,7 @@ public class WebSecurityConfig {
         auth
                 .inMemoryAuthentication()
                 .withUser("user")
-                .password("{noop}password")
+                .password(passwordEncoder.encode("password"))
                 .roles("USER");
         return auth;
     }
