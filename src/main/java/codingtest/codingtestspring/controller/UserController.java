7381d82d398923a7f1fe7f760ec7affc7ab07348
조태중 <jtj3926@gmail.com>
2023-12-11 15:49:37 +0900

    코드 작성

---
+++ src/main/java/codingtest/codingtestspring/controller/UserController.java
@@ -0,0 +1,35 @@
+// UserController.java
+package codingtest.codingtestspring.controller;
+
+import codingtest.codingtestspring.entity.User;
+import codingtest.codingtestspring.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/signup")
+    public User signup(@RequestBody User user) {
+        user.setCreatedAt(LocalDateTime.now());
+        return userRepository.save(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user, HttpSession session) {
+        Optional<User> foundUser = userRepository.findByUsername(user.getUsername());
+        if (foundUser.isPresent() && foundUser.get().getPassword().equals(user.getPassword())) {
+            session.setAttribute("user", foundUser.get());
+            return "Login successful";
+        } else {
+            return "Invalid username or password";
+        }
+    }
+}
--- src/main/java/codingtest/codingtestspring/repository/UserRepository.java
+++ src/main/java/codingtest/codingtestspring/repository/UserRepository.java
@@ -1,8 +1,18 @@
+// UserRepository.java
 package codingtest.codingtestspring.repository;
 
 import codingtest.codingtestspring.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    // 필요한 쿼리 메소드
+    default User signup(User user) {
+        // user.setPassword("temporaryPassword"); // 임시 비밀번호 설정
+        return save(user);
+    }
+
+    Optional<User> findByUsername(String username);
 }
